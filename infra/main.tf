d6955d4e087fb84f6931204bf8e13d0e055183ee
Hemant5hk <hemanthrajendran@gmail.com>
2025-07-18 22:23:42 +0530

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -8,7 +8,7 @@ resource "azurerm_resource_group" "example" {
 }
 
 resource "azurerm_storage_account" "example" {
-  name                     = "examplestoracc123"
+  name                     = "examplestoracc0123"
   resource_group_name      = azurerm_resource_group.example.name
   location                 = azurerm_resource_group.example.location
   account_tier             = "Standard"

846e694b19a254b705a5111e0d3349586f18831f
Hemant5hk <hemanthrajendran@gmail.com>
2025-07-18 22:26:46 +0530

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -2,15 +2,15 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
+resource "azurerm_resource_group" "hkresource" {
+  name     = "hktest-resources"
   location = "East US"
 }
 
 resource "azurerm_storage_account" "example" {
-  name                     = "examplestoracc0123"
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
+  name                     = "examplestoracc00123"
+  resource_group_name      = azurerm_resource_group.hkresource.name
+  location                 = azurerm_resource_group.hkresource.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
